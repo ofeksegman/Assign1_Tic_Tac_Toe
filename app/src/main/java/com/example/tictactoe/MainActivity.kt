7b3b00ab3975703072e35dc2681e18e6f180fe9e
NatiEzra <128730088+NatiEzra@users.noreply.github.com>
2024-12-12 10:40:08 +0200

    start of logic in kotlin

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +17,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
 
+        // Get the ImageButton by ID
+        val imageButton = findViewById<ImageButton>(R.id.imageButton00)
+
+        // Set a new image resource when clicked
+        imageButton.setOnClickListener {
+            imageButton.setImageResource(R.drawable.X_logo) // Replace 'new_image' with your drawable resource name
+    }
 }
+    }
